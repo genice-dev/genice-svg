621bce42f7dceb91f8b98cd6d67edbdefb83fc79
Masakazu Matsumoto <vitroid@gmail.com>
2019-10-05 01:38:58 +0900

    Trial use of jinja2.

--- replacer.py
+++ replacer.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 import sys
-from genice.tool import line_replacer
 from genice_svg.formats.svg import __doc__ as doc_svg
 from genice_svg.formats.png import __doc__ as doc_png
 import distutils.core
+import jinja2 as jj
 
 setup = distutils.core.run_setup("setup.py")
 
 d = {
-    "%%usage_svg%%"   : "\n".join(doc_svg.splitlines()[2:]),
-    "%%usage_png%%"   : "\n".join(doc_png.splitlines()[2:]),
-    "%%version%%" : setup.get_version(),
-    "%%package%%" : setup.get_name(),
-    "%%url%%"     : setup.get_url(),
-    "%%genice%%"  : "[GenIce](https://github.com/vitroid/GenIce)",
-    "%%requires%%": "\n".join(setup.install_requires),
+    "usage_svg"   : "\n".join(doc_svg.splitlines()[2:]),
+    "usage_png"   : "\n".join(doc_png.splitlines()[2:]),
+    "version" : setup.get_version(),
+    "package" : setup.get_name(),
+    "url"     : setup.get_url(),
+    "genice"  : "[GenIce](https://github.com/vitroid/GenIce)",
+    "requires": setup.install_requires,
 }
 
 
-for line in sys.stdin:
-    print(line_replacer(line, d), end="")
+print(jj.Template(sys.stdin.read()).render(d))
--- temp_README.md
+++ temp_README.md
@@ -3,19 +3,20 @@
     genice 4R -f png[shadow:rotatex=2:rotatey=88] > 4R.png
 
 
-# [genice-svg](%%url%%)
+# [genice-svg]({{url}})
 
 A [GenIce](https://github.com/vitroid/GenIce) plugin to illustrate the structure in SVG (and PNG) format.
 
-version %%version%%
+version {{version}}
 
 ## Requirements
-
-* %%requires%%
+{% for i in requires %}
+* {{i}}
+{%- endfor %}
 
 ## Installation from PyPI
 
-    % pip install %%package%%
+    % pip install {{package}}
 
 ## Manual Installation
 
@@ -29,11 +30,15 @@ Copy the files in genice_svg/formats/ into your local formats/ folder.
 
 ## Usage
 
-    %%usage_svg%%
+{%- filter indent %}
+    {{usage_svg}}
+{%- endfilter %}
 
 Png is a quick alternative for svg. Use png if making svg is too slow.
 
-    %%usage_png%%
+{%- filter indent %}
+    {{usage_png}}
+{%- endfilter %}
 
 ## Test in place
 
